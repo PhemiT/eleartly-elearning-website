ee569649d62bf19b6ef46145126a5cca999f75a2
Olubukunmi Olufemi <phemiwrites@gmail.com>
2022-03-30 14:42:01 +0100

    Faqs

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 // Navbar on Scroll Color Change.
 
 window.addEventListener('scroll', () => {
-    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 50)
+})
+
+// show/hide faq
+
+const faqs = document.querySelectorAll('.faq')
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open')
+
+        //Change Icon
+        const icon = document.querySelector('.faq__icon i')
+        if(icon.className === 'uil uil-plus') {
+            icon.className = 'uil uil-minus'
+        } else {
+            icon.className = 'uil uil-plus'
+        }
+    })
 })
